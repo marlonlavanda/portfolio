85f693c639bc1d0cd8ea84015dbb6848734ea275
Marlon Lavanda <marlonlavanda@hotmail.com>
2022-11-15 00:38:27 +0000

    change

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -2,8 +2,8 @@ import ProjectsGrid from "../../components/ProjectsGrid"
 
 export default function Projects() {
   return (
-    <section className="w-screen max-w-full h-[80vh] flex justify-center items-center">
-      <div className="container">
+    <section className="w-screen max-w-full flex justify-center items-center">
+      <div className="container px-6 lg:px-0">
         <h1 className="font-bold text-2xl mb-4">Selected Projects</h1>
         <ProjectsGrid />
       </div>
